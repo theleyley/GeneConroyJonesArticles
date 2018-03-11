3bdf2240d9b04caf274942927f5333c8816c437f
jbadillo <jbadillo@illumina.com>
2018-03-11 15:51:10 -0700

    new directive for embeded js

--- root/client/app/app.js
+++ root/client/app/app.js
@@ -50,6 +50,20 @@ ptApp.component('ptLogo',{
     }
 });
 
+ptApp.directive('script', function() {
+    return {
+        restrict: 'E',
+        scope: false,
+        link: function (scope, elem, attr) {
+            if (attr.type === 'text/javascript-lazy') {
+                var code = elem.text();
+                var f = new Function(code);
+                f();
+            }
+        }
+    };
+});
+
 // Configure paths to point to appropriate html templates and specify ui-sref='/someRandomName'
 ptApp.config(function($routeProvider, $locationProvider) {
     $routeProvider
